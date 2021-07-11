97d008d47d870cdc5c0e07a62368c660ffe89255
luvagu <luiavag@gmail.com>
2021-07-11 00:02:35 -0500

    added axios fetch answers and setter

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,60 @@ import axios from 'axios'
 import CardsList from './components/CardsList'
 import Header from './components/Header'
 
+const decodeString = (string) => {
+	const text = document.createElement('textarea')
+	text.innerHTML = string
+	return text.value
+}
+
+/**
+ * 
+ * @todo
+ * fix flip answer background
+ * add choose answer
+ * add scores for correct and incorrect answers  
+ */
+
 function App() {
 	const [isLoading, setIsLoading] = useState(false)
 	const [categories, setCategories] = useState([])
 	const [flashcards, setFlashcards] = useState([])
+	const [options, setOptions] = useState({ amount: '10' })
 
-	const handleChange = e => {}
+	const handleChange = e => {
+		setOptions(prevOptions => ({
+			...prevOptions,
+			[e.target.id]: e.target.value,
+		}))
+	}
 
-	const handleSubmit = e => {}
+	const handleSubmit = async e => {
+		e.preventDefault()
+
+		try {
+			const { data } = await axios({
+				method: 'GET',
+				url: 'https://opentdb.com/api.php',
+				params: options,
+			})
+
+			setFlashcards(data.results.map((questionItem, index) => {
+        const answer = decodeString(questionItem.correct_answer)
+        const wrongAnswers = [
+          ...questionItem.incorrect_answers.map(a => decodeString(a)),
+          answer
+        ]
+        return {
+          id: `${index}-${Date.now()}`,
+          question: decodeString(questionItem.question),
+          answer: answer,
+          options: wrongAnswers.sort(() => Math.random() - .5)
+        }
+      }))
+		} catch (error) {
+			console.log(error)
+		}
+	}
 
 	useEffect(() => {
 		setIsLoading(true)
