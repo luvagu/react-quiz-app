36a9064ca82e40f877cae2e79aa11ea2998d54f0
luvagu <luiavag@gmail.com>
2021-07-10 22:55:42 -0500

    added axios, fetching categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { Fragment, useState } from 'react'
+import { Fragment, useEffect, useState } from 'react'
+import axios from 'axios'
 import CardsList from './components/CardsList'
 import Header from './components/Header'
 
 function App() {
+	const [isLoading, setIsLoading] = useState(false)
 	const [categories, setCategories] = useState([])
 	const [flashcards, setFlashcards] = useState([])
 
 	const handleChange = e => {}
+
 	const handleSubmit = e => {}
+
+	useEffect(() => {
+		setIsLoading(true)
+		let cancel
+
+		axios({
+			method: 'GET',
+			url: 'https://opentdb.com/api_category.php',
+			cancelToken: new axios.CancelToken(c => (cancel = c)),
+		})
+			.then(({ data }) => {
+				setCategories(data.trivia_categories)
+				setIsLoading(false)
+			})
+			.catch(error => {
+				if (axios.isCancel(error)) return
+				console.log(error)
+				setIsLoading(false)
+			})
+
+		return () => cancel()
+	}, [])
 
 	return (
 		<Fragment>
@@ -17,7 +42,7 @@ function App() {
 				handleSubmit={handleSubmit}
 			/>
 			<div className='container'>
-				<CardsList flashcards={flashcards}  />
+				<CardsList flashcards={flashcards} />
 			</div>
 		</Fragment>
 	)
