2f01201e7ac474449265f56a4cead54f9cc8e20c
luvagu <luiavag@gmail.com>
2021-07-10 22:43:26 -0500

    changed returned method

--- src/components/CardsList.jsx
+++ src/components/CardsList.jsx
@@ -3,9 +3,9 @@ import FlashCard from './FlashCard'
 function CardsList({ flashcards }) {
 	return (
 		<div className='card-grid'>
-			{flashcards.map(flashcard => {
-				return <FlashCard flashcard={flashcard} key={flashcard.id} />
-			})}
+			{flashcards.map(flashcard => (
+				<FlashCard flashcard={flashcard} key={flashcard.id} />
+			))}
 		</div>
 	)
 }
